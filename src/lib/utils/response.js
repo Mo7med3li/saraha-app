f0b2a6122807d7332cc6fa1d977588b08b723df5
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-07-18 03:25:55 +0300

    feat : add utility functions for handling asynchronous requests , success response and global error responses

---
+++ src/lib/utils/response.js
@@ -0,0 +1,24 @@
+export const asyncHandler = (func) => {
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next);
+    } catch (error) {
+      error.cause = 500;
+      return next(error);
+    }
+  };
+};
+
+export const globalErrorHandler = (err, req, res, next) => {
+  return res.status(err.cause || 400).json({
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+export const successResponse = ({ res, statusCode, message, data }) => {
+  return res.status(statusCode || 200).json({
+    message: message || "Success",
+    data: data || null,
+  });
+};
