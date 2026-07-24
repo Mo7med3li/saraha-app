73407277bf486eda1c25eab8afe9a282e160e93c
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-07-24 05:42:52 +0300

    refactor: simplify authMiddleware by utilizing decodeToken for user retrieval and dynamic token type handling

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -1,36 +1,20 @@
 import { findById } from "../db/db.service.js";
 import UserModel from "../db/models/user.model.js";
-import { ROLES_ENUM } from "../lib/constants/constants.js";
+import { ROLES_ENUM, TOKEN_TYPES_ENUM } from "../lib/constants/constants.js";
 import { asyncHandler } from "../lib/utils/response.js";
 import {
+  decodeToken,
   getSignature,
   verifyToken,
 } from "../lib/utils/security/token.security.js";
 
-export const authMiddleware = () => {
+export const authMiddleware = ({
+  tokenType = TOKEN_TYPES_ENUM.ACCESS,
+} = {}) => {
   return asyncHandler(async (req, res, next) => {
     const { authorization } = req.headers;
 
-    const [bearer, token] = authorization?.split(" ") || [];
-    if (!token || !bearer) {
-      return next(new Error("token is required", { cause: 401 }));
-    }
-
-    // get the signature based on the bearer level
-    const { accessSignature } = getSignature({ bearer });
-    const decodedToken = verifyToken({ token, signature: accessSignature });
-    if (!decodedToken?._id) {
-      return next(new Error("invalid token", { cause: 401 }));
-    }
-    const { _id } = decodedToken;
-    const user = await findById({
-      model: UserModel,
-      id: _id,
-      select: "-password",
-    });
-    if (!user) {
-      return next(new Error("user not found", { cause: 404 }));
-    }
+    const user = await decodeToken({ next, authorization, tokenType });
     req.user = user;
     next();
   });
