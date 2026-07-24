247df85b380eb033215c7942bc74e7c2d0f99ccb
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-07-24 04:52:32 +0300

    feat: update JWT secret handling to use environment variables for user and system signatures, and add utility to retrieve access and refresh signatures

--- src/lib/utils/security/token.security.js
+++ src/lib/utils/security/token.security.js
@@ -1,13 +1,28 @@
 import jwt from "jsonwebtoken";
+import { SIGNATURE_LEVEL_LABEL } from "../../constants/constants.js";
 
 export const generateToken = ({
   payload = {},
-  signature = process.env.JWT_SECRET,
+  signature = process.env.USER_JWT_SECRET,
   options = { expiresIn: 60 * 60 },
 }) => {
   return jwt.sign(payload, signature, options);
 };
 
-export const verifyToken = ({ token, signature = process.env.JWT_SECRET }) => {
+export const verifyToken = ({
+  token,
+  signature = process.env.USER_JWT_SECRET,
+}) => {
   return jwt.verify(token, signature);
+};
+
+export const getSignature = ({ bearer = SIGNATURE_LEVEL_LABEL.BEARER }) => {
+  const signatureLevel =
+    bearer === SIGNATURE_LEVEL_LABEL.BEARER ? "USER" : "SYSTEM";
+  const accessSignature =
+    process.env[`${signatureLevel}_JWT_SECRET`] || process.env.USER_JWT_SECRET;
+  const refreshSignature =
+    process.env[`${signatureLevel}_REFRESH_JWT_SECRET`] ||
+    process.env.USER_REFRESH_JWT_SECRET;
+  return { accessSignature, refreshSignature };
 };
