daae0624a002eb0390e48ddecfdfd5a50138c99f
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-06 03:59:17 +0300

    feat: implement Google authentication with login and signup services

--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { TOKEN_TYPES_ENUM } from "../../../lib/constants/constants.js";
 import { authMiddleware } from "../../../middleware/authentication.middleware.js";
-import { login, refreshToken, signup } from "../services/auth.service.js";
+import {
+  googleLogin,
+  googleSignup,
+  login,
+  refreshToken,
+  signup,
+} from "../services/auth.service.js";
 
 const authRouter = Router();
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
+authRouter.post("/signup-google", googleSignup);
+authRouter.post("/login-google", googleLogin);
+
 authRouter.get(
   "/refresh-token",
   authMiddleware({ tokenType: TOKEN_TYPES_ENUM.REFRESH }),
--- src/modules/auth/services/auth.service.js
+++ src/modules/auth/services/auth.service.js
@@ -1,3 +1,4 @@
+import { OAuth2Client } from "google-auth-library";
 import { createOne, findOne } from "../../../db/db.service.js";
 import UserModel from "../../../db/models/user.model.js";
 import { asyncHandler, successResponse } from "../../../lib/utils/response.js";
@@ -7,6 +8,7 @@ import {
   hashPassword,
 } from "../../../lib/utils/security/hash.security.js";
 import { generateTokens } from "../../../lib/utils/security/token.security.js";
+import { PROVIDERS_ENUM } from "../../../lib/constants/constants.js";
 
 // Signup service
 export const signup = asyncHandler(async (req, res, next) => {
@@ -53,7 +55,7 @@ export const login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
   const user = await findOne({
     model: UserModel,
-    filters: { email },
+    filters: { email, providers: PROVIDERS_ENUM.SYSTEM },
   });
 
   if (!user) {
@@ -86,5 +88,87 @@ export const refreshToken = asyncHandler(async (req, res, next) => {
     statusCode: 200,
     message: "Refresh token generated successfully",
     data: { accessToken, refreshToken },
+  });
+});
+
+// verify google token
+async function verifyGoogleToken({ idToken }) {
+  const client = new OAuth2Client();
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience: process.env.GOOGLE_OAUTH_CLIENT_ID.split(","),
+  });
+  const payload = ticket.getPayload();
+
+  return payload;
+}
+
+// google login service
+export const googleLogin = asyncHandler(async (req, res, next) => {
+  const { idToken } = req.body;
+
+  const { email, email_verified } = await verifyGoogleToken({
+    idToken,
+  });
+  if (!email_verified) {
+    return next(new Error("email not verified", { cause: 400 }));
+  }
+
+  const user = await findOne({
+    model: UserModel,
+    filters: { email, providers: PROVIDERS_ENUM.GOOGLE },
+  });
+  if (!user) {
+    return next(new Error("invalid provider or invalid email", { cause: 404 }));
+  }
+  const { accessToken, refreshToken } = await generateTokens({ user });
+  return successResponse({
+    res,
+    statusCode: 200,
+    message: "Login successful",
+    data: { accessToken, refreshToken },
+  });
+});
+
+// google signup service
+export const googleSignup = asyncHandler(async (req, res, next) => {
+  const { idToken } = req.body;
+
+  const { email, name, picture, email_verified } = await verifyGoogleToken({
+    idToken,
+  });
+  if (!email_verified) {
+    return next(new Error("email not verified", { cause: 400 }));
+  }
+
+  const user = await findOne({
+    model: UserModel,
+    filters: { email },
+  });
+  if (user) {
+    return next(new Error("email already exists", { cause: 409 }));
+  }
+
+  const [firstName, lastName] = name.split(" ");
+  if (await findOne({ model: UserModel, filters: { firstName, lastName } })) {
+    return next(new Error("user name already exists", { cause: 409 }));
+  }
+  const newUser = await createOne({
+    model: UserModel,
+    data: [
+      {
+        email,
+        userName: name,
+        confirmEmail: new Date(),
+        picture,
+        providers: PROVIDERS_ENUM.GOOGLE,
+      },
+    ],
+  });
+  return successResponse({
+    res,
+    statusCode: 201,
+    message: "User created successfully",
+    data: { userId: newUser[0].id },
   });
 });
