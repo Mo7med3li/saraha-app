a1a95af24306723e3e88d3e1933f0e05ad640cfa
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-08 23:54:53 +0300

    feat: add endpoint to fetch user shared data with validation middleware

--- src/modules/users/controller/users.controller.js
+++ src/modules/users/controller/users.controller.js
@@ -3,8 +3,10 @@ import {
   authMiddleware,
   authorizeMiddleware,
 } from "../../../middleware/authentication.middleware.js";
-import { getUserById } from "../services/users.service.js";
+import { getUserById, getUserSharedData } from "../services/users.service.js";
 import { authorize } from "../../../authorize/authorize.js";
+import { userSharedDataSchema } from "../schema/user.schema.js";
+import { validationMiddleware } from "../../../middleware/validation.middleware.js";
 
 const usersRouter = Router();
 
@@ -13,6 +15,11 @@ usersRouter.get(
   authMiddleware(),
   authorizeMiddleware({ roles: authorize.profile }),
   getUserById,
+);
+usersRouter.get(
+  "/:id",
+  validationMiddleware(userSharedDataSchema),
+  getUserSharedData,
 );
 
 export default usersRouter;
--- src/modules/users/services/users.service.js
+++ src/modules/users/services/users.service.js
@@ -1,3 +1,5 @@
+import { findOne } from "../../../db/db.service.js";
+import UserModel from "../../../db/models/user.model.js";
 import { asyncHandler, successResponse } from "../../../lib/utils/response.js";
 import { decryption } from "../../../lib/utils/security/encryption.security.js";
 
@@ -12,6 +14,28 @@ export const getUserById = asyncHandler(async (req, res, next) => {
     res,
     statusCode: 200,
     message: "User fetched successfully",
+    data: { user },
+  });
+});
+
+export const getUserSharedData = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await findOne({
+    model: UserModel,
+    filters: { _id: id, confirmEmail: { $exists: true } },
+    select: "-password -confirmEmailOtpAttempts",
+  });
+  if (!user) {
+    return next(new Error("User not found", { cause: 404 }));
+  }
+  user.phoneNumber = decryption({
+    cipherText: user.phoneNumber,
+  });
+
+  return successResponse({
+    res,
+    statusCode: 200,
+    message: "User data fetched successfully",
     data: { user },
   });
 });
