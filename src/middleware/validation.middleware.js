63a7a5215ccbed3fd25a0b17693b798b75e5e895
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-08 03:08:03 +0300

    feat: update validation middleware to return structured error response and refactor Joi schemas for user authentication

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -4,7 +4,10 @@ export const validationMiddleware = (schema) => {
   return asyncHandler(async (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return next(new Error(error.message, { cause: 400 }));
+      return res.status(400).json({
+        error: "validation error",
+        message: error.message,
+      });
     }
     return next();
   });
--- src/modules/auth/schema/auth.schema.js
+++ src/modules/auth/schema/auth.schema.js
@@ -1,16 +1,8 @@
 import Joi from "joi";
 import { Gender_Enum } from "../../../lib/constants/constants.js";
 
-export const signupSchema = Joi.object().keys({
-  userName: Joi.string()
-    .trim()
-    .min(2)
-    .max(30)
-    .regex(/^[a-zA-Z\u0621-\u064Aء-ئ][^#&<>\"~;$^%{}?]{1,20}$/)
-    .required()
-    .messages({
-      "string.pattern.base": "Invalid user name format",
-    }),
+// Login schema
+export const loginSchema = Joi.object().keys({
   email: Joi.string()
     .email({
       tlds: { allow: ["com", "net", "org", "in"] },
@@ -27,6 +19,19 @@ export const signupSchema = Joi.object().keys({
       "string.pattern.base":
         "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
     }),
+});
+
+// Signup schema
+export const signupSchema = loginSchema.append({
+  userName: Joi.string()
+    .trim()
+    .min(2)
+    .max(30)
+    .regex(/^[a-zA-Z\u0621-\u064Aء-ئ][^#&<>\"~;$^%{}?]{1,20}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid user name format",
+    }),
   confirmPassword: Joi.string().required().valid(Joi.ref("password")).messages({
     "any.only": "Password and confirm password do not match",
   }),
@@ -37,15 +42,4 @@ export const signupSchema = Joi.object().keys({
       "string.pattern.base": "Invalid phone number format",
     }),
   gender: Joi.string().valid(Gender_Enum.MALE, Gender_Enum.FEMALE).required(),
-});
-
-export const loginSchema = Joi.object().keys({
-  email: Joi.string()
-    .email({
-      tlds: { allow: ["com", "net", "org", "in"] },
-      minDomainSegments: 2,
-      maxDomainSegments: 3,
-    })
-    .required(),
-  password: Joi.string().trim().min(8).required(),
 });
