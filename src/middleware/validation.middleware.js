4bb8deeb7c05602fa5a4aaae20208f5f9ea7f137
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-08 04:02:27 +0300

    refactor: enhance validation middleware to support multiple schema validations and return structured error messages

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -2,13 +2,20 @@ import { asyncHandler } from "../lib/utils/response.js";
 
 export const validationMiddleware = (schema) => {
   return asyncHandler(async (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
+    const validationErrors = [];
+    for (const keys of Object.keys(schema)) {
+      const { error } = schema[keys].validate(req[keys]);
+      if (error) {
+        validationErrors.push({ keys, message: error.message });
+      }
+    }
+    if (validationErrors.length > 0) {
       return res.status(400).json({
         error: "validation error",
-        message: error.message,
+        message: validationErrors,
       });
     }
+
     return next();
   });
 };
