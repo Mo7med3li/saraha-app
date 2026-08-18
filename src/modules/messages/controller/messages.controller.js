12dbdd6f2125c0bf82362853228ea0eabdbd428d
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-19 01:03:56 +0300

    feat: implement message creation endpoint with file upload and validation for sender and receiver

--- src/modules/messages/controller/messages.controller.js
+++ src/modules/messages/controller/messages.controller.js
@@ -1,9 +1,30 @@
 import { Router } from "express";
+import { createMessage } from "../services/message.service.js";
+import { cloudinaryFileUpload } from "../../../lib/utils/multer/cloud.multer.js";
+import { FILE_FILTER_VALIDATION } from "../../../lib/constants/constants.js";
+import { validationMiddleware } from "../../../middleware/validation.middleware.js";
+import { createMessageSchema } from "../schema/message.schema.js";
+import { authMiddleware } from "../../../middleware/authentication.middleware.js";
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) => {
-  res.send("Hello World");
-});
+messagesRouter.post(
+  "/:receiverId",
+  cloudinaryFileUpload({
+    filterValidation: FILE_FILTER_VALIDATION.image,
+  }).array("attachments", 2),
+  validationMiddleware(createMessageSchema),
+  createMessage,
+);
+
+messagesRouter.post(
+  "/:receiverId/sender",
+  authMiddleware(),
+  cloudinaryFileUpload({
+    filterValidation: FILE_FILTER_VALIDATION.image,
+  }).array("attachments", 2),
+  validationMiddleware(createMessageSchema),
+  createMessage,
+);
 
 export default messagesRouter;
