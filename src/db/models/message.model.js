ea746ed6965f276b5d3a6a52f12d274e9e2b12f5
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-19 01:03:16 +0300

    feat: create message model schema for handling user messages

---
+++ src/db/models/message.model.js
@@ -0,0 +1,46 @@
+import Mongoose from "mongoose";
+import {
+  Gender_Enum,
+  PROVIDERS_ENUM,
+  ROLES_ENUM,
+} from "../../lib/constants/constants.js";
+
+const messageSchema = new Mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      minLength: [3, "Content must be at least 3 characters long"],
+      maxLength: [20000, "Content must be less than 20000 characters long"],
+      required: function () {
+        return this.attachments.length === 0;
+      },
+    },
+    attachments: [
+      {
+        imageUrl: String,
+        asset_id: String,
+      },
+    ],
+    senderId: {
+      type: Mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiverId: {
+      type: Mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+const MessageModel =
+  Mongoose.models.Message || Mongoose.model("Message", messageSchema);
+
+MessageModel.syncIndexes();
+
+export default MessageModel;
