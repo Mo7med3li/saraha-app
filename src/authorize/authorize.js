06619b598c224604f68ce8421a5d2cec0b7d2b13
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-26 05:31:26 +0300

    refactor: remove deprecated authorization, database connection, and user management files to streamline codebase

--- src/authorize/authorize.js
+++
@@ -1,7 +0,0 @@
-import { ROLES_ENUM } from "../lib/constants/constants.js";
-
-export const authorize = {
-  profile: [ROLES_ENUM.USER, ROLES_ENUM.ADMIN],
-  restoreAccount: [ROLES_ENUM.ADMIN],
-  deleteAccount: [ROLES_ENUM.ADMIN],
-};
