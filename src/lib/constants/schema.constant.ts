2a1cd5e5c302e2da279e586802737c14c0aa25a1
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-08-30 14:00:14 +0300

    feat: add authorization and database connection modules, and define constants for user roles and validation schemas

---
+++ src/lib/constants/schema.constant.ts
@@ -0,0 +1,84 @@
+import Joi, { LanguageMessages } from "joi";
+import { Gender_Enum, LOGOUT_ENUM } from "./constants.js";
+import { Types } from "mongoose";
+
+export const generalFieldsSchema = {
+  userName: Joi.string()
+    .trim()
+    .regex(/^[A-Za-z\u0621-\u064A]{3,20} [A-Za-z\u0621-\u064A]{3,20}$/)
+    .messages({
+      "string.pattern.base":
+        "User name must be first and last name separated by one space (3-20 letters each)",
+    }),
+  email: Joi.string()
+    .trim()
+    .lowercase()
+    .email({
+      tlds: {
+        allow: [
+          "com",
+          "net",
+          "org",
+          "edu",
+          "gov",
+          "io",
+          "co",
+          "eg",
+          "info",
+          "me",
+          "dev",
+        ],
+      },
+      minDomainSegments: 2,
+      maxDomainSegments: 3,
+    })
+    .messages({
+      "string.email": "Please enter a valid email address",
+      "string.empty": "Email is required",
+    }),
+  password: Joi.string()
+    .trim()
+    .min(8)
+    .max(64)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,64}$/)
+    .messages({
+      "string.min": "Password must be at least 8 characters",
+      "string.max": "Password must be at most 64 characters",
+      "string.pattern.base":
+        "Password must include uppercase, lowercase, number, and special character",
+    }),
+  confirmPassword: Joi.string().valid(Joi.ref("password")).messages({
+    "any.only": "Password and confirm password do not match",
+  }),
+  phoneNumber: Joi.string()
+    .regex(/^(002|\+2)?01[0125][0-9]{8}$/)
+
+    .messages({
+      "string.pattern.base": "Invalid phone number format",
+    }),
+  gender: Joi.string().valid(...Object.values(Gender_Enum)),
+  otp: Joi.string().min(6).max(6).messages({
+    "string.min": "OTP must be 6 digits",
+    "string.max": "OTP must be 6 digits",
+  }),
+  id: Joi.string().custom((value, helpers) => {
+    return (
+      Types.ObjectId.isValid(value) ||
+      helpers.message("invalid id format" as unknown as LanguageMessages)
+    );
+  }),
+  flag: Joi.string()
+    .valid(...Object.values(LOGOUT_ENUM))
+    .default(LOGOUT_ENUM.STAY_LOGGED_IN),
+  file: {
+    fieldname: Joi.string().required(),
+    originalname: Joi.string().required(),
+    encoding: Joi.string().required(),
+    destination: Joi.string().required(),
+    mimetype: Joi.string().required(),
+    finalPath: Joi.string().required(),
+    filename: Joi.string().required(),
+    path: Joi.string().required(),
+    size: Joi.number().positive().required(),
+  },
+};
